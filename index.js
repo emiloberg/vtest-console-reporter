49748327445db30da3ea973ee2971320cbedd1bc
Emil Öberg <emil.oberg@monator.com>
2017-02-10 08:57:34 +0100

    Broken into result and test reporter

--- index.js
+++ index.js
@@ -1,69 +1,44 @@
 const path = require("path")
 
-function log(str = "") {
-  console.log(`   ${str}`)
-}
+function result({ specs, config, chalk, log }) {
+  const totalTests = specs.flatSpecs.length
+  const failedTests = specs.flatSpecs
+    .filter(spec => spec.passTest === false)
+    .length
+  const totalComponents = specs.groupedSpecs.length
+  const failedComponents = specs.groupedSpecs
+    .filter(spec => spec.passTest === false)
+    .length
+  const badge = failedComponents === 0
+    ? chalk.bgGreen.white(` PASSED ALL ${totalComponents} COMPONENTS `)
+    : chalk.bgRed.white(` FAILED ${failedComponents} OF ${totalComponents} COMPONENTS `)
 
-function report({ specs, config, chalk }) {
-  let failedComponents = 0
-  log()
-
-  specs.groupedSpecs.forEach(component => {
-    log(`${component.passTest ? chalk.white.bgGreen(" PASS ") : chalk.white.bgRed(" FAIL ")}   ${component.id}`)
-
-    if (!component.passTest) {
-      failedComponents += 1
-      component.variants.forEach(variant => {
-        const passVariant = variant.resolutions.every(resolution => (
-          resolution.screenshots.every(screenshot => screenshot.passTest)
-        ))
-
-        if (!passVariant) {
-          log(`           Props variant: ${variant.name}`)
-          variant.resolutions.forEach(resolution => {
-            resolution.screenshots.forEach(screenshot => {
-              if (!screenshot.passTest) {
-                log(`             ${screenshot.browser}`
-                  + ` - ${screenshot.resolution.width}x${screenshot.resolution.height}`)
-                log(`               Mismatch %: ${chalk.bold(screenshot.misMatchPercentage)}`)
-                if (screenshot.masterScreenshotExists) {
-                  log(`               Diff: ${
-                    chalk.bold(
-                      path.join(config.get("temp.screenshots.diff"), screenshot.screenshotFilename)
-                    )}`)
-                } else {
-                  log(`               ${chalk.bold(
-                    "Screenshot to compare against does not exist!"
-                  )}`)
-                }
-              }
-            })
-          })
-        }
-      })
-    }
-  })
-
-  log()
-  if (failedComponents === 0) {
-    log(chalk.bgGreen.white(` PASSED ALL ${specs.groupedSpecs.length} TESTS `))
-  } else {
-    log(
-      chalk.bgRed.white(
-        ` FAILED ${failedComponents} OF ${specs.groupedSpecs.length} COMPONENTS `
-      )
-    )
-  }
-  log()
-
-  console.log()
-  console.log(chalk.blue("   Screenshots available"))
-  console.log(chalk.blue("      New: ") + config.get("temp.screenshots.new"))
-  console.log(chalk.blue("      Diff: ") + config.get("temp.screenshots.diff"))
-  console.log(chalk.blue("      Master: ") + config.get("temp.screenshots.master"))
-  console.log()
+  log.log()
+  log.log(badge)
+  log.log(`Performed ${totalTests} tests, failed ${failedTests}` )
+  log.log()
+  log.info("Screenshots available")
+  log.infoValue("   New", config.get("temp.screenshots.new"))
+  log.infoValue("   Diff", config.get("temp.screenshots.diff"))
+  log.infoValue("   Master", config.get("temp.screenshots.master"))
 
   return specs
 }
 
-module.exports = report
+function test({ specs, chalk, log }) {
+  const badge = specs.passTest
+    ? chalk.white.bgGreen(" PASS ")
+    : chalk.white.bgRed(" FAIL ")
+  const missMatch = specs.masterScreenshotExists
+    ? `Mismatch %: ${chalk.bold(specs.misMatchPercentage)}`
+    : "No master screenshot"
+  const resolution = `${specs.resolution.width}x${specs.resolution.height}`
+
+  log.log(`${badge}   ${specs.spec.id}`)
+  log.log(`           ${specs.variantName} | ${specs.browser} | ${resolution} | ${missMatch}`)
+}
+
+module.exports = {
+  result,
+  test
+}
